f86e0c3a227c217f69eddab6069bd14ed6a5c352
uthsa <uthsavi29@gmail.com>
2021-01-09 00:43:33 +0530

    Added:To Check If Last Name Starts With Capital And Has Minimum Three Characters

--- user_registration.py
+++ user_registration.py
@@ -2,7 +2,7 @@
   @Author: Uthsavi KP
   @Date: 2021-01-08 23:27:24
   @Last Modified by:  Uthsavi KP
-  @Last Modified time: 2021-01-08 23:27:24
+  @Last Modified time: 2021-01-09 00:42:58
   @Title: User Registration Problem 
 '''
 
@@ -14,6 +14,13 @@ class UserRegistration:
         and validating user input using these patterns
         """
         self.name = "^[A-Z][a-zA-Z]{2,}$"
+
+    def get_user_registration(self):
+        """
+        contains all the methods 
+        """
+        user_register.get_first_name() 
+        user_register.get_last_name()
 
     def get_first_name(self):    
         """
@@ -29,8 +36,24 @@ class UserRegistration:
                 print("Invalid first name,Re enter your name with capital and with minimum 3 characters ")	
                 self.get_first_name()
         except Exception as err:
-            print(err)        
+            print(err)    
+
+    def get_last_name(self):
+        """
+        getting input for last name and
+        checking if last name starts with 
+        capital and has minimum 3 characters
+        """
+        try:
+            last_name_input = input("Enter your last name :")
+            if re.match(self.name, last_name_input):
+                print("Valid last name")
+            else:
+                print("Invalid last name,Re enter your name with capital and with minimum 3 characters ")	
+                self.get_last_name()
+        except Exception as err:
+            print(err)            
 
 if __name__ == "__main__":
     user_register = UserRegistration()
-    user_register.get_first_name()        
+    user_register.get_user_registration()
